eeb322eb47d37e95862d5045cedeec853d4a0220
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-04-08 14:36:29 +0200

    Job06

---
+++ Job06/Job06_2.ino
@@ -0,0 +1,15 @@
+// Synchronous LED flicker
+void setup() {
+  pinMode(2, OUTPUT); // LED 1
+  pinMode(3, OUTPUT); // LED 2
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(500); // on for 500 ms
+
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  delay(500); // off for 500 ms
+}
